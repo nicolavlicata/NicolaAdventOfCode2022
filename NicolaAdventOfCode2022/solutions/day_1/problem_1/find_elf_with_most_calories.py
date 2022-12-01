c5fda6c1998b36dbc06308a2eec441f4d260d986
Nicola Licata <nlicata@amazon.com>
2022-12-01 10:22:42 -0800

    Day 1 solutions

---
+++ NicolaAdventOfCode2022/solutions/day_1/problem_1/find_elf_with_most_calories.py
@@ -0,0 +1,19 @@
+import math
+
+
+def find_elf_with_most_calories(calories):
+    max_so_far = -math.inf
+    current_sum = 0
+    for line in calories:
+        if line == "":
+            max_so_far = max(max_so_far, current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(line)
+    max_so_far = max(max_so_far, current_sum)
+    return max_so_far
+
+
+lines = open("../input.txt").read().splitlines()
+print(find_elf_with_most_calories(lines))
+
---
+++ NicolaAdventOfCode2022/solutions/day_1/problem_2/find_top_3_elves_with_most_calories.py
@@ -0,0 +1,23 @@
+import heapq
+
+
+def find_top_3_elves_with_most_calories(calories):
+    max_heap = []
+    current_sum = 0
+    for line in calories:
+        if line == "":
+            heapq.heappush(max_heap, -1 * current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(line)
+    heapq.heappush(max_heap, -1 * current_sum)
+    top_3 = 0
+    counter = 3
+    while counter > 0 and max_heap:
+        top_3 += -1 * heapq.heappop(max_heap)
+        counter -= 1
+    return top_3
+
+
+lines = open("../input.txt").read().splitlines()
+print(find_top_3_elves_with_most_calories(lines))
